d47a3266e7598e47f8b0bad4ad4a0cd1517eebf6
Glendon Small <gsmall@sonovate.com>
2019-08-07 00:42:52 +0100

    This is  funny

--- application/resources/views/welcome.blade.php
+++ application/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Inc
                 </div>
 
                 <div class="links">
